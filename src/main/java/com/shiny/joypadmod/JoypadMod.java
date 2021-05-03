b8653c9d0f1369b2cc9e38813eb3a3d3c6abe255
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-05-03 02:18:40 -0400

    Update for 1.12.2

--- src/main/java/com/shiny/joypadmod/JoypadMod.java
+++ src/main/java/com/shiny/joypadmod/JoypadMod.java
@@ -23,12 +23,12 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = JoypadMod.MODID, name = JoypadMod.NAME, version = JoypadMod.VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.12]")
+@Mod(modid = JoypadMod.MODID, name = JoypadMod.NAME, version = JoypadMod.VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.12,1.12.2]")
 public class JoypadMod {
     public static Logger logger = LogManager.getLogger("Joypad Mod");
     public static final String MODID = "joypadsplitscreenmod";
     public static final String NAME = "Joypad Mod";
-    public static final String VERSION = "1.12-14.21.0.2387";
+    public static final String VERSION = "1.12.2-14.23.5.2847";
 
     private static ControllerSettings controllerSettings;
 
